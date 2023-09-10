4675a10a04747fcee0196883f7b462bb78a4c7be
Mozibix <opuzmoses@gmail.com>
2023-09-10 20:09:59 +0100

    chores: omo scatter scatter version 4

--- app/api/address/create/route.ts
+++ app/api/address/create/route.ts
@@ -3,7 +3,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-export async function POST(req: { json: () => Promise<any> }) {
+export async function POST(req) {
   const supabase = createServerComponentClient({ cookies });
 
   try {
