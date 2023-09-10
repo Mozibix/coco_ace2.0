fdf568d800bbe931d3176ecd7875a4d919f80ed4
Mozibix <opuzmoses@gmail.com>
2023-09-10 19:20:31 +0100

    chores: updated products  component

--- app/(components)/Products.tsx
+++ app/(components)/Products.tsx
@@ -12,31 +12,34 @@ const Product: React.FC<IProp> = ({ product }) => {
     <>
       <Link
         href={`/product/${product?.id}`}
-        className="max-w-[200px] p-1.5 border border-gray-50 hover:border-gray-200 hover:shadow-xl bg-gray-100 rounded mx-auto"
+        className="w-[20rem] h-[26rem] overflow-hidden p-1.5 border border-gray-50 hover:border-gray-200 hover:shadow-xl bg-gray-100 rounded mx-auto"
       >
         {product?.url ? (
           <img
-            className="rounded cursor-pointer"
-            src={product.url + "/190"}
-            alt="product"
+            className="rounded cursor-pointer w-full h-[20rem]"
+            src={product.url}
+            alt={product?.title}
           />
-        ) : null}
+        ) : (
+          <></>
+        )}
 
-        <div className="pt-2 px-1">
-          <div className="font-semibold text-[15px] hover:underline cursor-pointer">
+        <div className="pt-10 px-1">
+          <div className="font-semibold text-center text-[1rem] hover:underline cursor-pointer">
             {product?.title}
           </div>
+          {/*
           <div className="font-extrabold">
             ${(product?.price / 100).toFixed(2)}
           </div>
 
-          <div className="relative flex items-center text-[12px] text-gray-500">
+           <div className="relative flex items-center text-[12px] text-gray-500">
             <div className="line-through">
               ${((product?.price * 1.2) / 100).toFixed(2)}
             </div>
             <div className="px-2">-</div>
             <div className="line-through">20%</div>
-          </div>
+          </div> */}
         </div>
       </Link>
     </>
