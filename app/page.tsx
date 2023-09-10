4ba39604d7853a8339f91506e3569fa52c59a94e
Mozibix <opuzmoses@gmail.com>
2023-09-10 17:41:41 +0100

    chores: updated home page component

--- app/page.tsx
+++ app/page.tsx
@@ -32,13 +32,17 @@ const Home = () => {
     <MainLayout>
       <CarouselComp />
 
-      <div className="max-w-[1200px] mx-auto">
-        <div className="text-2xl font-bold mt-4 mb-6 px-4">Products</div>
+      <div className="products_sec">
+        <div>
+          <div className="text-2xl lg:text-3xl uppercase font-bold mt-4 mb-6 px-4">
+            Products
+          </div>
 
-        <div className="grid grid-cols-5 gap-4">
-          {products.map((product) => (
-            <Product key={product.id} product={product} />
-          ))}
+          <div className="products_container">
+            {products.map((product) => (
+              <Product key={product.id} product={product} />
+            ))}
+          </div>
         </div>
       </div>
     </MainLayout>
