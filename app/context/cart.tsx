852ea3bc81018a5b83942709bfb21e39ca5bc5a7
Mozibix <opuzmoses@gmail.com>
2023-09-10 21:50:57 +0100

    chores: shege pro max commit 9

--- app/context/cart.tsx
+++ app/context/cart.tsx
@@ -1,85 +1,97 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { createContext, useState, useContext } from "react";
+import { useRouter } from "next/router"; // Updated import path
+import { createContext, useState, useContext, useEffect } from "react"; // Added useEffect for initial load
 
-const Context = createContext();
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  // Add other properties as needed
+}
 
-const Provider = ({ children }) => {
+interface CartItem extends Product {
+  quantity: number;
+}
+
+interface CartContextType {
+  isItemAdded: boolean;
+  getCart: () => CartItem[];
+  addToCart: (product: Product) => void;
+  removeFromCart: (product: Product) => void;
+  isItemAddedToCart: (product: Product) => void;
+  cartCount: () => number;
+  cartTotal: () => number;
+  clearCart: () => void;
+}
+
+const Context = createContext<CartContextType | undefined>(undefined);
+
+const Provider: React.FC = ({ children }) => {
+  // Added React.FC for children
   const router = useRouter();
 
-  const [isItemAdded, setIsItemAdded] = useState(false);
+  const [isItemAdded, setIsItemAdded] = useState<boolean>(false);
 
-  const getCart = () => {
-    let cart = [];
+  useEffect(() => {
+    isItemAddedToCart(); // Initialize isItemAdded on component load
+  }, []);
+
+  const getCart = (): CartItem[] => {
+    let cart: CartItem[] = [];
     if (typeof localStorage !== "undefined") {
-      cart = JSON.parse(localStorage.getItem("cart")) || [];
+      cart = JSON.parse(localStorage.getItem("cart") || "[]");
     }
     return cart;
   };
 
-  const addToCart = (product) => {
-    let cart = [];
-    if (typeof localStorage !== "undefined") {
-      cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const addToCart = (product: Product): void => {
+    let cart: CartItem[] = getCart();
+    const existingItem = cart.find((item) => item.id === product.id);
+
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      cart.push({ ...product, quantity: 1 });
     }
-    cart.push(product);
+
     localStorage.setItem("cart", JSON.stringify(cart));
     isItemAddedToCart(product);
-    router.refresh();
+    router.reload(); // Updated router refresh method
   };
 
-  const removeFromCart = (product) => {
-    let cart = [];
-    if (typeof localStorage !== "undefined") {
-      cart = JSON.parse(localStorage.getItem("cart")) || [];
-    }
+  const removeFromCart = (product: Product): void => {
+    let cart: CartItem[] = getCart();
     cart = cart.filter((item) => item.id !== product.id);
     localStorage.setItem("cart", JSON.stringify(cart));
     isItemAddedToCart(product);
-    router.refresh();
+    router.reload(); // Updated router refresh method
   };
 
-  const isItemAddedToCart = (product) => {
-    let cart = [];
-    if (typeof localStorage !== "undefined") {
-      cart = JSON.parse(localStorage.getItem("cart")) || [];
-    }
-    cart = cart.filter((item) => item.id === product.id);
+  const isItemAddedToCart = (product?: Product): void => {
+    let cart: CartItem[] = getCart();
 
-    if (cart.length > 0) {
-      setIsItemAdded(true);
-      return;
+    if (product) {
+      const found = cart.some((item) => item.id === product.id);
+      setIsItemAdded(found);
+    } else {
+      setIsItemAdded(cart.length > 0);
     }
-
-    setIsItemAdded(false);
   };
 
-  const cartCount = () => {
-    let cart = [];
-    if (typeof localStorage !== "undefined") {
-      cart = JSON.parse(localStorage.getItem("cart")) || [];
-    }
-    return cart.length;
+  const cartCount = (): number => {
+    let cart: CartItem[] = getCart();
+    return cart.reduce((total, item) => total + item.quantity, 0);
   };
 
-  const cartTotal = () => {
-    let total = 0;
-    let cart = [];
-    if (typeof localStorage !== "undefined") {
-      cart = JSON.parse(localStorage.getItem("cart")) || [];
-    }
-    for (let i = 0; i < cart.length; i++) {
-      const element = cart[i];
-      total += element.price;
-    }
-
-    return total;
+  const cartTotal = (): number => {
+    let cart: CartItem[] = getCart();
+    return cart.reduce((total, item) => total + item.price * item.quantity, 0);
   };
 
-  const clearCart = () => {
+  const clearCart = (): void => {
     localStorage.removeItem("cart");
-    router.refresh();
+    router.reload(); // Updated router refresh method
   };
 
   const exposed = {
@@ -96,6 +108,12 @@ const Provider = ({ children }) => {
   return <Context.Provider value={exposed}>{children}</Context.Provider>;
 };
 
-export const useCart = () => useContext(Context);
+export const useCart = (): CartContextType => {
+  const context = useContext(Context);
+  if (context === undefined) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+  return context;
+};
 
 export default Provider;
