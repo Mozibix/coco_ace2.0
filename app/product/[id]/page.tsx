ed43089acbc819e834995c5f2904f86b5c895792
Mozibix <opuzmoses@gmail.com>
2023-09-10 22:55:08 +0100

    chores: shege pro max commit 14

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,111 +1,126 @@
 "use client";
 
+// Import the required modules and components
 import React, { useEffect, useState } from "react";
-import SimilarProducts from "@/app/(components)/SimilarProducts";
-import MainLayout from "@/app/layout/MainLayout";
 import { useCart } from "@/app/context/cart";
 import { toast } from "react-toastify";
+import MainLayout from "@/app/layout/MainLayout";
+import SimilarProducts from "@/app/(components)/SimilarProducts";
 import UseIsLoading from "@/app/hooks/UseIsLoading";
 
+// Define the Product type
 interface Product {
   id: number;
   title: string;
   description: string;
   url: string;
   price: number;
+  name?: string; // Add the 'name' property if it's required
 }
 
+// Define the ProductProps interface
 interface ProductProps {
   params: { id: number };
 }
 
 export default function Product({ params }: ProductProps) {
   const cart = useCart();
-
-  const [product, setProduct] = useState<Product | {}>({});
+  const [product, setProduct] = useState<Product | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const getProduct = async () => {
-    UseIsLoading(true);
-    setProduct({} as Product);
+    setIsLoading(true);
 
-    const response = await fetch(`/api/product/${params.id}`);
-    const prod: Product = await response.json();
-    setProduct(prod);
-    cart.isItemAddedToCart(prod);
-    UseIsLoading(false);
+    try {
+      const response = await fetch(`/api/product/${params.id}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch product data");
+      }
+
+      const prod: Product = await response.json();
+      setProduct(prod);
+    } catch (error) {
+      console.error(error);
+      // Handle error here, e.g., show an error message
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
     getProduct();
   }, [params.id]);
 
+  const addToCart = () => {
+    if (product) {
+      if (cart.isItemAdded) {
+        cart.removeFromCart(product);
+        toast.info("Removed from cart", { autoClose: 3000 });
+      } else {
+        cart.addToCart(product);
+        toast.success("Added to cart", { autoClose: 3000 });
+      }
+    }
+  };
+
   return (
-    <>
-      <MainLayout>
-        <div className="products_id_sec">
-          <div className="lg:flex px-4 py-10">
-            {product?.url ? (
+    <MainLayout>
+      <div className="products_id_sec">
+        <div className="lg:flex px-4 py-10">
+          {product && (
+            <img
+              className="products_id_sec_image"
+              src={product.url}
+              alt={product.title}
+            />
+          )}
+
+          <div className="px-4 mt w-full">
+            {isLoading ? (
+              <p>Loading...</p>
+            ) : product ? (
               <>
-                <img
-                  className="products_id_sec_image"
-                  src={`${product?.url}`}
-                  alt={product?.title}
-                />
-              </>
-            ) : (
-              <>
-                <div className="w-[10%]"></div>
-              </>
-            )}
+                <div className="font-bold text-xl">{product.title}</div>
+                <div className="text-sm text-gray-700 pt-2">Brand New</div>
 
-            <div className="px-4 mt w-full">
-              <div className="font-bold text-xl">{product?.title}</div>
-              <div className="text-sm text-gray-700 pt-2">Brand New</div>
+                <div className="border-b py-1" />
 
-              <div className="border-b py-1" />
-
-              <div className="pt-3 pb-2">
-                <div className="flex items-center">
-                  Condition:{" "}
-                  <span className="font-bold text-[17px] ml-2">New</span>
+                <div className="pt-3 pb-2">
+                  <div className="flex items-center">
+                    Condition:{" "}
+                    <span className="font-bold text-[17px] ml-2">New</span>
+                  </div>
                 </div>
-              </div>
 
-              <div className="border-b py-1" />
+                <div className="border-b py-1" />
 
-              <div className="pt-3">
-                <div className="w-full text-sm sm:text-lg flex items-center justify-between">
-                  <button
-                    onClick={() => {
-                      if (cart.isItemAdded) {
-                        cart.removeFromCart(product as Product);
-                        toast.info("Removed from cart", { autoClose: 3000 });
-                      } else {
-                        cart.addToCart(product as Product);
-                        toast.success("Added to cart", { autoClose: 3000 });
-                      }
-                    }}
-                    className={`
-                      text-white py-2 px-20 rounded-full cursor-pointer 
-                      ${
-                        cart.isItemAdded
+                <div className="pt-3">
+                  <div className="w-full text-sm sm:text-lg flex items-center justify-between">
+                    <button
+                      onClick={addToCart}
+                      className={`text-white py-2 px-20 rounded-full cursor-pointer ${
+                        cart.isItemAdded(product)
                           ? "bg-[#e9a321] hover:bg-[#bf851a]"
                           : "bg-[#3498C9] hover:bg-[#0054A0]"
-                      }
-                    `}
-                  >
-                    {cart.isItemAdded ? "Remove From Cart" : "Add To Cart"}
-                  </button>
+                      }`}
+                    >
+                      {cart.isItemAdded(product)
+                        ? "Remove From Cart"
+                        : "Add To Cart"}
+                    </button>
+                  </div>
                 </div>
-              </div>
 
-              <div className="border-b py-1" />
-            </div>
+                <div className="border-b py-1" />
+              </>
+            ) : (
+              <p>Product not found.</p>
+            )}
           </div>
         </div>
+      </div>
 
-        <SimilarProducts />
-      </MainLayout>
-    </>
+      <SimilarProducts />
+    </MainLayout>
   );
 }
