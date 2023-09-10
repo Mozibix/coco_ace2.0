eaa71c98f5ee44361f789a58263c546cce23db79
Mozibix <opuzmoses@gmail.com>
2023-09-10 20:27:59 +0100

    shege seen successfully

--- app/hooks/UseCreateAddress.ts
+++ app/hooks/UseCreateAddress.ts
@@ -1,30 +1,30 @@
-const UseCreateAddress = async (details: {
-  addressId: any;
-  name: any;
-  address: any;
-  zipcode: any;
-  city: any;
-  country: any;
-}) => {
-  let url = "create";
-  if (details.addressId) url = "update";
+// const UseCreateAddress = async (details: {
+//   addressId: any;
+//   name: any;
+//   address: any;
+//   zipcode: any;
+//   city: any;
+//   country: any;
+// }) => {
+//   let url = "create";
+//   if (details.addressId) url = "update";
 
-  const response = await fetch(`/api/address/${url}`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      addressId: details.addressId,
-      name: details.name,
-      address: details.address,
-      zipcode: details.zipcode,
-      city: details.city,
-      country: details.country,
-    }),
-  });
+//   const response = await fetch(`/api/address/${url}`, {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({
+//       addressId: details.addressId,
+//       name: details.name,
+//       address: details.address,
+//       zipcode: details.zipcode,
+//       city: details.city,
+//       country: details.country,
+//     }),
+//   });
 
-  const data = await response.json();
+//   const data = await response.json();
 
-  return data;
-};
+//   return data;
+// };
 
-export default UseCreateAddress;
+// export default UseCreateAddress;
